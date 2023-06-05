40d91fcf84b734036e2aed12b26fb2f26b003800
arungoudk <akondam@cisco.com>
2023-06-05 09:35:46 +0000

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+name = "pods5-vm"
+owner = "pods5-student"
+env = "SRE-LAB"
+key_pair_name = "sre_pods5_key"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+  default = "pods5-vm-01"
+}
+
+variable "env" {
+  type = string
+  default = "SRE-LAB"
+}
+
+variable "owner" {
+  type = string
+  default = "pods5"
+}
+
+variable "key_pair_name" {
+  type = string
+  default = "sre_pods5_key"
+}
